ef0ad6f41ba09148a0cc7bc3cdf7c97b03709b6a
demandajmenso <demandajmenso@gmail.com>
2021-12-02 14:47:10 +0200

    [fix] check if options dict exists

--- prestapyt/prestapyt.py
+++ prestapyt/prestapyt.py
@@ -567,19 +567,20 @@ class PrestaShopWebServiceDict(PrestaShopWebService):
 
         elems = dive(response, level=2)
         # There is no key attrs if we use display option so we return response as is
-        if 'display' in options:
-            if not elems:
-                return []
-            else:
-                return elems
+        if isinstance(options, dict):
+            if 'display' in options:
+                if not elems:
+                    return []
+                else:
+                    return elems
+
+        if not elems:
+            return []
+        elif isinstance(elems, list):
+            ids = [int(elem['attrs']['id']) for elem in elems]
         else:
-            if not elems:
-                return []
-            elif isinstance(elems, list):
-                ids = [int(elem['attrs']['id']) for elem in elems]
-            else:
-                # when there is only 1 resource, we do not have a list in the response
-                ids = [int(elems['attrs']['id'])]
+            # when there is only 1 resource, we do not have a list in the response
+            ids = [int(elems['attrs']['id'])]
         return ids
 
     def get_with_url(self, url):
